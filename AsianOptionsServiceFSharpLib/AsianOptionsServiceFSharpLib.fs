debd05a8aea8229e205fe6934f4bd43e30a812a7
ingted <anibal.yeh@gmail.com>
2013-05-09 00:19:47 +0800

    delete old projects

--- AsianOptionsServiceFSharpLib/AsianOptionsServiceFSharpLib.fs
+++
@@ -1,4 +0,0 @@
-﻿namespace AsianOptionsServiceFSharpLib
-
-type AsianOptionsService() = 
-    static member PriceAsianOptions(initial:double, exercise, double up, double down, double interest, int periods, int runs) = "F#";
