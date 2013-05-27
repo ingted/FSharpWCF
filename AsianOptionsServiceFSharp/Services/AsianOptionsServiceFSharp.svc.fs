fd66d7c935b9656c349705b4e6f06d9400b899d4
ingted <anibal.yeh@gmail.com>
2013-05-27 05:48:02 -0700

    FSharp version ok

--- AsianOptionsServiceFSharp/Services/AsianOptionsServiceFSharp.svc.fs
+++ AsianOptionsServiceFSharp/Services/AsianOptionsServiceFSharp.svc.fs
@@ -7,34 +7,42 @@ type AsianOptionsFSharp() =
     interface IAsianOptionsServiceFSharp with
         override this.PriceAsianOptions(initial:float, exercise:float, up:float
             , down:float, interest:float, periods:int32, runs:int32) = 
-            7788.0;
-            (*
+            
+            (*let initial = 30.0;;
+            let exercise = 30.0;;
+            let up = 1.4;;
+            let down = 0.8;;
+            let interest = 1.08;;
+            let periods = 20;;
+            let runs = 1000000;;*)
+
             let pricePath = Array.create (periods + 1) 0.0
             let piup = (interest - down) / (up - down)
-            let pidown = 1.0 - piup
+            let pidown = ref (1.0 - piup)
             let temp = ref 0.0
             let rand = new System.Random()
-            let priceAverage = 0.0;
-            let callPayOff = 0.0;
+            let priceAverage = 0.0
+            let callPayOff = 0.0
             {0..(runs - 1)} |> Seq.iter (
                 fun _ ->
                     let sumPricePath = ref 0.0
                     sumPricePath.Value <- initial  
-                    {1..periods} |> Seq.iteri (
-                        fun i _ -> 
+                    
+                    {1..periods} |> Seq.iter (
+                        fun i -> 
                             pricePath.[0] <- initial
                             let rn = rand.NextDouble()
                             match rn with
-                            | x when x > pidown ->
+                            | x when x > pidown.Value ->
                                 pricePath.[i] <- pricePath.[i - 1] * up
                             | _ ->
                                 pricePath.[i] <- pricePath.[i - 1] * down
-                            sumPricePath.Value <- sumPricePath.Value + pricePath.[i]
-                    )
+                            sumPricePath.Value <- sumPricePath.Value + pricePath.[i] //*)
+                    );
                     let priceAverage = sumPricePath.Value / (float periods + 1.0) * 1.0
-                    let callPayOff = Math.Max((priceAverage - exercise), 0.0)
+                    let callPayOff = System.Math.Max((priceAverage - exercise), 0.0)
                     temp.Value <- temp.Value + callPayOff
             )
 
-            (temp.Value / Math.Pow(interest, float periods)) / (float runs);
-            *)
+            (temp.Value / System.Math.Pow(interest, float periods)) / (float runs);
+            //*)
